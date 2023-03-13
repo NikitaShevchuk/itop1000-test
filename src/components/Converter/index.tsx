202d8a1af318cce23c02957c386d708d8b05c875
Nikita Shevhcuk <shevchuk.nikita.gh@gmail.com>
2023-03-13 11:27:33 +0200

    Remove console.log

--- src/components/Converter/index.tsx
+++ src/components/Converter/index.tsx
@@ -1,10 +1,10 @@
 import { ConverterContext } from '@/context/ConverterProvider'
 import React from 'react'
 import { Input } from '../Input'
+import { useGetCurrencies } from '../Input/hooks/useGetCurrencies'
 import style from './converter.module.scss'
 import { useSetAmount } from './hooks/useSetAmount'
 import { useSetCurrency } from './hooks/useSetCurrency'
-import { useGetCurrencies } from '../Input/hooks/useGetCurrencies'
 
 export const Converter = () => {
     const state = React.useContext(ConverterContext)
@@ -13,7 +13,6 @@ export const Converter = () => {
     const setFirstCurrency = useSetCurrency('first')
     const setSecondCurrency = useSetCurrency('second')
     useGetCurrencies()
-    console.log(state)
     return (
         <div className={style.converter}>
             <div className={style.converter__column}>
